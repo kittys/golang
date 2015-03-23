7d5ffaabe5855d5592e14dd6ea599c1c4eeb50a2
kittys <gampolt@live.com>
2015-03-23 16:03:21 +0700

    Add main.go: sample for use reflect to read http.Request

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+)
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hello World")
+	k := reflect.TypeOf(*r)
+	v := reflect.ValueOf(*r)
+	for i := 0; i < v.NumField(); i++ {
+		fmt.Fprintf(w, "%v : %v\n", k.Field(i), v.Field(i).Interface())
+	}
+}
